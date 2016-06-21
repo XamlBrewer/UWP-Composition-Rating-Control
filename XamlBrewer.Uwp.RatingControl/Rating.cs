d37b4019b4e46ec3acfbabec445b3cd5ee6653e4
Diederik Krols <diederik_krols@hotmail.com>
2016-06-21 21:32:50 +0200

    Basic template and Tapped handler.

--- XamlBrewer.Uwp.RatingControl/Rating.cs
+++ XamlBrewer.Uwp.RatingControl/Rating.cs
@@ -1,59 +1,86 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Shapes;
 
 namespace XamlBrewer.Uwp.Controls
 {
+    [TemplatePart(Name = ItemsPartName, Type = typeof(StackPanel))]
+    [TemplatePart(Name = InteractionPartName, Type = typeof(Grid))]
     public class Rating : Control
     {
+        private const string ItemsPartName = "PART_Items";
+        private const string InteractionPartName = "PART_Surface";
+
         #region Dependency Property Registrations
 
         public static readonly DependencyProperty MaximumProperty = DependencyProperty.Register(
             nameof(Maximum),
             typeof(int),
             typeof(Rating),
-            new PropertyMetadata(5));
+            new PropertyMetadata(5, OnStructureChanged));
 
         public static readonly DependencyProperty ValueProperty = DependencyProperty.Register(
             nameof(Value),
             typeof(double),
             typeof(Rating),
-            new PropertyMetadata(0d));
+            new PropertyMetadata(0d, OnValueChanged));
+
+        public static readonly DependencyProperty StepFrequencyProperty = DependencyProperty.Register(
+            nameof(StepFrequency),
+            typeof(double),
+            typeof(Rating),
+            new PropertyMetadata(1d, OnStructureChanged));
 
         public static readonly DependencyProperty EmptyImageProperty = DependencyProperty.Register(
             nameof(EmptyImage),
             typeof(Uri),
             typeof(Rating),
-            new PropertyMetadata(null));
+            new PropertyMetadata(null, OnStructureChanged));
 
         public static readonly DependencyProperty FilledImageProperty = DependencyProperty.Register(
             nameof(FilledImage),
             typeof(Uri),
             typeof(Rating),
-            new PropertyMetadata(null));
+            new PropertyMetadata(null, OnStructureChanged));
 
         public static readonly DependencyProperty ItemHeightProperty = DependencyProperty.Register(
             nameof(ItemHeight),
             typeof(int),
             typeof(Rating),
-            new PropertyMetadata(12));
+            new PropertyMetadata(12, OnStructureChanged));
 
         public static readonly DependencyProperty ItemPaddingProperty = DependencyProperty.Register(
             nameof(ItemPadding),
             typeof(int),
             typeof(Rating),
-            new PropertyMetadata(2));
+            new PropertyMetadata(2, OnStructureChanged));
 
         #endregion
+
+        public Rating()
+        {
+            this.DefaultStyleKey = typeof(Rating);
+        }
 
         public int Maximum
         {
             get { return (int)GetValue(MaximumProperty); }
             set { SetValue(MaximumProperty, value); }
+        }
+
+        public double StepFrequency
+        {
+            get { return (double)GetValue(StepFrequencyProperty); }
+            set { SetValue(StepFrequencyProperty, value); }
         }
 
         public double Value
@@ -92,7 +119,19 @@ namespace XamlBrewer.Uwp.Controls
         protected override void OnApplyTemplate()
         {
             OnStructureChanged(this);
+
+            var surface = this.GetTemplateChild(InteractionPartName) as Grid;
+            if (surface != null)
+            {
+                surface.Tapped += Surface_Tapped;
+            }
+
             base.OnApplyTemplate();
+        }
+
+        private void Surface_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            Value = (int)(e.GetPosition(this).X / ActualWidth * Maximum) + 1;
         }
 
         private static void OnStructureChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
@@ -103,6 +142,22 @@ namespace XamlBrewer.Uwp.Controls
         private static void OnStructureChanged(DependencyObject d)
         {
             Rating c = (Rating)d;
+
+            var panel = c.GetTemplateChild(ItemsPartName) as StackPanel;
+            if (panel != null)
+            {
+                for (int i = 0; i < c.Maximum; i++)
+                {
+                    panel.Children.Add(
+                        new Grid
+                        {
+                            Background = new SolidColorBrush(Colors.BlueViolet),
+                            Height = c.ItemHeight,
+                            Width = c.ItemHeight,
+                            Margin = new Thickness(0, 0, c.ItemPadding, 0)
+                        });
+                }
+            }
 
             // ...
 
@@ -117,6 +172,12 @@ namespace XamlBrewer.Uwp.Controls
         private static void OnValueChanged(DependencyObject d)
         {
             Rating c = (Rating)d;
+
+            var panel = c.GetTemplateChild(ItemsPartName) as StackPanel;
+            if (panel != null)
+            {
+
+            }
         }
     }
 }
